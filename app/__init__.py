23d5d3b4d54b9bba91447f1d1dfd2ec5f1d0cbcb
JYB <7303639+BingoJYB@users.noreply.github.com>
2020-03-01 14:16:29 +0100

    use import_string for extension and blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import import_string
 
-
-# Globally accessible libraries
-db = SQLAlchemy()
+extensions = [
+    'app.models:db'
+]
+blueprints = [
+    'app.routes:my_app'
+]
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    # Initialize Plugins
-    db.init_app(app)
-
-    from app.routes import my_app
-    # Register Blueprints
-    app.register_blueprint(my_app)
+    for extension in extensions:
+        ext = import_string(extension)
+        ext.init_app(app)
+        
+    for blueprint in blueprints:
+        bp = import_string(blueprint)
+        app.register_blueprint(bp)
 
     return app
