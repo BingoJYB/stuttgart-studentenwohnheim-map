7c3d6327f4680444eddabb42fd6507380909b17d
JYB <7303639+BingoJYB@users.noreply.github.com>
2020-09-28 21:09:13 +0200

    add OpenLayers map

--- app/libs/utilities.py
+++ app/libs/utilities.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import os
 
 from flask import current_app
 
@@ -8,8 +8,9 @@ def get_image_url(image_url):
 
 
 def get_downloaded_image_path(wohnung_name):
-    return current_app.config['IMAGE_FOLDER'] + '/' + wohnung_name
+    return current_app.config['IMAGE_FOLDER'] + wohnung_name
 
 
 def check_image_folder_exist():
-    Path(current_app.config['IMAGE_FOLDER']).mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(current_app.config['IMAGE_FOLDER']):
+        os.makedirs(current_app.config['IMAGE_FOLDER'])
