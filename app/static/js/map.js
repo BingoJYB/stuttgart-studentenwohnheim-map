382c8cb9477cca0d2a117399ddf23bf87b945847
JYB <7303639+BingoJYB@users.noreply.github.com>
2020-10-09 02:02:08 +0200

    add markers on the map

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,10 +1,10 @@
-let moveMapToPos = (map, latitude, longitude) => {
-  map.setCenter({ lat: latitude, lng: longitude });
-  map.setZoom(12);
-};
+let name_position_pair = {};
 
-let onError = (error) => {
-  alert(error.message);
+let addMarkersToMap = (map) => {
+  for (const [wohnung_name, position] of Object.entries(name_position_pair)) {
+    console.log(position);
+    map.addObject(new H.map.Marker({ lat: position[0], lng: position[1] }));
+  }
 };
 
 //Step 1: initialize communication with the platform
@@ -35,17 +35,27 @@ let ui = H.ui.UI.createDefault(map, defaultLayers);
 // Get an instance of the geocoding service:
 let service = platform.getSearchService();
 
-service.geocode(
-  {
-    q: "Stuttgart",
-  },
-  (result) => {
-    console.log(result);
-    moveMapToPos(
-      map,
-      result.items[0].position.lat,
-      result.items[0].position.lng
-    );
-  },
-  onError
-);
+let promises = [];
+
+for (const [wohnung_name, address] of Object.entries(name_address_pair)) {
+  promises.push(
+    service.geocode(
+      {
+        q: address,
+      },
+      (result) => {
+        name_position_pair[wohnung_name] = [
+          result.items[0].position.lat,
+          result.items[0].position.lng,
+        ];
+      },
+      (error) => {
+        alert(error.message);
+      }
+    )
+  );
+}
+
+Promise.all(promises).then((values) => {
+  addMarkersToMap(map);
+});
